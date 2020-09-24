619da491d861c9ddeef9ff20a721cbec92335c52
bbhatia17 <bbhatia17@gmail.com>
2020-09-24 19:58:10 +0200

    Added Constants in Util Package

---
+++ src/main/java/com/orderhandling/util/OrderHandlingConstants.java
@@ -0,0 +1,9 @@
+package com.orderhandling.util;
+
+public class OrderHandlingConstants {
+    public static final String PRODUCT_TYPE_BOOK = "book";
+    public static final String PRODUCT_TYPE_VIDEO = "video";
+    public static final String PRODUCT_TYPE_PHYSICAL = "physical";
+    public static final String PAYMENT_STATUS = "paid";
+
+}
